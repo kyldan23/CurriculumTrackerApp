96f048e1440c81c05d724ef980145347522ae1be
Lillian Weng <lillianweng@lillians-air.lan>
2021-05-21 21:54:49 -0700

    frontend basic format

--- frontend/tracker/src/App.js
+++ frontend/tracker/src/App.js
@@ -3,22 +3,54 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+    <header> Plextech Full-Stack Curriculum Course Schedule 
+    <div class="grid-container">
+      <div class="grid-label">Lecture Number</div>
+      <div class="grid-label">Topic</div>
+      <div class="grid-label">Video</div>
+      <div class="grid-item">1</div>
+      <div class="grid-item">HTML</div>
+      <div class="grid-item">6</div>
+
+      <div class="grid-item">2</div>
+      <div class="grid-item">CSS</div>
+      <div class="grid-item">9</div>
+
+      <div class="grid-item">3</div>
+      <div class="grid-item">JavaScript 1</div>
+      <div class="grid-item">6</div>
+
+      <div class="grid-item">4</div>
+      <div class="grid-item">JavaScript 2</div>
+      <div class="grid-item">9</div>
+      
+      <div class="grid-item">5</div>
+      <div class="grid-item">React</div>
+      <div class="grid-item">6</div>
+
+      <div class="grid-item">6</div>
+      <div class="grid-item">NodeJS</div>
+      <div class="grid-item">9</div>
+
+      <div class="grid-item">7</div>
+      <div class="grid-item">Firebase</div>
+      <div class="grid-item">6</div>
+
+      <div class="grid-item">8</div>
+      <div class="grid-item">Google Cloud Platform</div>
+      <div class="grid-item">9</div>
+
+      <div class="grid-item">9</div>
+      <div class="grid-item">Django</div>
+      <div class="grid-item">9</div>
+
+      <div class="grid-item">10</div>
+      <div class="grid-item">Flask + Deployment</div>
+      <div class="grid-item">9</div>
+  </div>
+  </header> 
+  </body>
   );
 }
 
