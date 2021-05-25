5e9670714a45a9df3bd5193011d2c9c280fc3aec
Lillian Weng <lillianweng@lillians-air.lan>
2021-05-24 22:23:26 -0700

    login ugly frontend

--- frontend/tracker/src/Login.js
+++ frontend/tracker/src/Login.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './Login.css';
 
 export default function Login() {
+  
   return(
     <header>Plextech Full-Stack Curriculum
-      <h1>Login</h1>
+      <h1>Login</h1> 
+      <form>
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
     </header>
+    
   );
 }
