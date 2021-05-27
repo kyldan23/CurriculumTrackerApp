1a8a8e6dda2ffef3f1f9a6dfa8047826d9598ee4
Lillian Weng <lillianweng@lillians-air.lan>
2021-05-26 19:11:41 -0700

    input vidoes

--- frontend/tracker/src/Home.js
+++ frontend/tracker/src/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.css';
 import YtTemplate from './YtTemplate'
 import YouTube from 'react-youtube';
+
 export default function Home() {
   return(
     <body>
@@ -13,44 +14,61 @@ export default function Home() {
       <div class="grid-item">1</div>
       <div class="grid-item">HTML</div>
       <div class="grid-item">
-        <YtTemplate vidId = 'TlB_eWDSMt4'/>
+        <YtTemplate vidId = 'UB1O30fR-EE'/>
       </div>
 
       <div class="grid-item">2</div>
       <div class="grid-item">CSS</div>
-      <div class="grid-item">9</div>
+      <div class="grid-item">
+        <YtTemplate vidId = 'yfoY53QXEnI'/></div>
 
       <div class="grid-item">3</div>
-      <div class="grid-item">JavaScript 1</div>
-      <div class="grid-item">6</div>
+      <div class="grid-item">JavaScript</div> 
+      {/* originally "Javascript 1" but we found 1 video total */}
+      <div class="grid-item">
+      <YtTemplate vidId = 'hdI2bqOjy3c'/>
+      </div>
 
-      <div class="grid-item">4</div>
+      {/* <div class="grid-item">4</div>
       <div class="grid-item">JavaScript 2</div>
-      <div class="grid-item">9</div>
+      <div class="grid-item">
+        <YtTemplate vidId = 'TlB_eWDSMt4'/>
+      </div> */}
       
       <div class="grid-item">5</div>
       <div class="grid-item">React</div>
-      <div class="grid-item">6</div>
+      <div class="grid-item">
+        <YtTemplate vidId = 'w7ejDZ8SWv8'/>
+      </div>
 
       <div class="grid-item">6</div>
       <div class="grid-item">NodeJS</div>
-      <div class="grid-item">9</div>
+      <div class="grid-item">
+        <YtTemplate vidId = 'TlB_eWDSMt4'/></div>
 
       <div class="grid-item">7</div>
       <div class="grid-item">Firebase</div>
-      <div class="grid-item">6</div>
+      <div class="grid-item">
+        <YtTemplate vidId = '9kRgVxULbag'/>
+      </div>
 
       <div class="grid-item">8</div>
       <div class="grid-item">Google Cloud Platform</div>
-      <div class="grid-item">9</div>
+      <div class="grid-item">
+        <YtTemplate vidId = 'vACTtmLWiQY'/>
+      </div>
 
       <div class="grid-item">9</div>
       <div class="grid-item">Django</div>
-      <div class="grid-item">9</div>
+      <div class="grid-item">
+        <YtTemplate vidId = 'e1IyzVyrLSU'/>
+      </div>
 
       <div class="grid-item">10</div>
       <div class="grid-item">Flask + Deployment</div>
-      <div class="grid-item">9</div>
+      <div class="grid-item">
+        <YtTemplate vidId = 'YFBRVJPhDGY'/>
+      </div>
   </div>
   </header> 
   </body>
