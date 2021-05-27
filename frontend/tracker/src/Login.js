45683cc63b6edd2a87053387098c8f4393e5c8b2
Jash Garish <jashgarish@gmail.com>
2021-05-26 20:14:09 -0700

    FireBase

--- frontend/tracker/src/Login.js
+++ frontend/tracker/src/Login.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './Login.css';
-
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
+import { config } from "./test-credentials";
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/signedIn',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+  ],
+};
+function SignInScreen() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <p>Please sign-in:</p>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
+}
 export default function Login() {
   return(
     <header>Plextech Full-Stack Curriculum
       <h1>Login</h1>
+      <SignInScreen/>
     </header>
   );
 }
---
+++ frontend/tracker/src/test-credentials.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const config = {
+    apiKey: "AIzaSyA1yUpdL86r0YJWRuGYBwklJbfLE0vm9RE",
+    authDomain: "tracker-3d28b.firebaseapp.com",
+    databaseURL: "https://tracker-3d28b-default-rtdb.firebaseio.com",
+    projectId: "tracker-3d28b",
+    storageBucket: "tracker-3d28b.appspot.com",
+    messagingSenderId: "651745283874",
+    appId: "1:651745283874:web:f96a42785c76271f395f7b",
+    measurementId: "G-N5GYQ5S7X3"
+  };
