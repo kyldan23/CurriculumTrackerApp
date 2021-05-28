f78f5f85f7765c6fb6f1c783f690d9a6575a2de4
Lillian Weng <lillianweng@lillians-air.lan>
2021-05-27 20:55:16 -0700

    button whack

--- tracker/src/Home.js
+++ tracker/src/Home.js
@@ -13,63 +13,106 @@ export default function Home() {
       <div class="grid-item">1</div>
       <div class="grid-item">HTML</div>
       <div class="grid-video">
-        <YtTemplate vidId = 'UB1O30fR-EE'/>
+        <button class="accordion">Show Video</button>
+        <div class="panel">
+        <YtTemplate vidId = 'UB1O30fR-EE'/></div>
       </div>
 
       <div class="grid-item">2</div>
       <div class="grid-item">CSS</div>
       <div class="grid-video">
+        <button class="accordion">Show Video</button>
+        <div class="panel">
         <YtTemplate vidId = 'yfoY53QXEnI'/></div>
+      </div>
 
       <div class="grid-item">3</div>
       <div class="grid-item">JavaScript</div> 
-      {/* originally "Javascript 1" but we found 1 video total */}
       <div class="grid-video">
-      <YtTemplate vidId = 'hdI2bqOjy3c'/>
+        <button class="accordion">Show Video</button>
+        <div class="panel">
+        <YtTemplate vidId = 'hdI2bqOjy3c'/></div>
       </div>
-
-      {/* <div class="grid-item">4</div>
-      <div class="grid-item">JavaScript 2</div>
-      <div class="grid-item">
-        <YtTemplate vidId = 'TlB_eWDSMt4'/>
-      </div> */}
       
       <div class="grid-item">5</div>
       <div class="grid-item">React</div>
       <div class="grid-video">
-        <YtTemplate vidId = 'w7ejDZ8SWv8'/>
+        <button class="accordion">Show Video</button>
+        <div class="panel">
+        <YtTemplate vidId = 'w7ejDZ8SWv8'/></div>
       </div>
 
       <div class="grid-item">6</div>
       <div class="grid-item">NodeJS</div>
       <div class="grid-video">
-        <YtTemplate vidId = 'TlB_eWDSMt4'/></div>
+        <button class="accordion">Show Video</button>
+        <div class="panel">
+        <YtTemplate vidId = 'TlB_eWDSMt4'/></div></div>
 
       <div class="grid-item">7</div>
       <div class="grid-item">Firebase</div>
       <div class="grid-video">
-        <YtTemplate vidId = '9kRgVxULbag'/>
+        <button class="accordion">Show Video</button>
+        <div class="panel">
+        <YtTemplate vidId = '9kRgVxULbag'/></div>
       </div>
 
       <div class="grid-item">8</div>
       <div class="grid-item-two-lines">Google Cloud Platform</div>
       <div class="grid-video">
-        <YtTemplate vidId = 'vACTtmLWiQY'/>
+        <button class="accordion">Show Video</button>
+        <div class="panel">
+        <YtTemplate vidId = 'vACTtmLWiQY'/></div>
       </div>
 
       <div class="grid-item">9</div>
       <div class="grid-item">Django</div>
       <div class="grid-video">
-        <YtTemplate vidId = 'e1IyzVyrLSU'/>
+        <button class="accordion">Show Video</button>
+        <div class="panel">
+        <YtTemplate vidId = 'e1IyzVyrLSU'/></div>
       </div>
 
       <div class="grid-item">10</div>
       <div class="grid-item-two-lines">Flask + Deployment</div>
       <div class="grid-video">
-        <YtTemplate vidId = 'YFBRVJPhDGY'/>
+        <button class="accordion">Show Video</button>
+        <div class="panel">
+        <YtTemplate vidId = 'YFBRVJPhDGY'/></div>
       </div>
   </div>
   </header> 
+
+  <script>
+    {/* document.querySelectorAll('.video-button').forEach(button => {
+      button.addEventListener('click', () => {
+        const videoContent = button.nextElementSibling;
+
+        button.classList.toggle('video-button--active');
+        
+        if (button.classList.contains('video-button--active')){
+          videoContent.style.maxHeight = videoContent.scrollHeight + 'px';
+        } else{
+          videoContent.style.maxHeight = 0;
+        }
+      })
+    }) */}
+  </script>
   </body>
   );
 }
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
