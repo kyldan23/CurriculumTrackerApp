de649ddec113d88db9bfe60e7c35801436085312
Kyle Dang <justsogaming253@gmail.com>
2021-05-28 12:15:34 -0700

    figured out how to check for berkeley email

--- tracker/src/Login.js
+++ tracker/src/Login.js
@@ -1,8 +1,9 @@
-import React from 'react';
+//import React from 'react';
 import './Login.css';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
-
+import React, { useEffect, useState } from 'react'; //testing 
+/*
 const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: 'popup',
@@ -34,3 +35,54 @@ export default function Login() {
     </h2>
   );
 }
+*/
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false,
+  },
+};
+function SignInScreen() {
+  const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+
+  // Listen to the Firebase Auth state and set the local state.
+  useEffect(() => {
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      if (user && user.email.includes("@berkeley.edu")) {
+        setIsSignedIn(true);
+      }
+      else{
+        setIsSignedIn(false);
+      }
+    });
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+  }, []);
+
+  if (!isSignedIn) {
+    return (
+      <div>
+        <h1>My App</h1>
+        <p>Please sign-in:</p>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>My App</h1>
+      <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+      <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+    </div>
+  );
+}
+
+export default SignInScreen;
